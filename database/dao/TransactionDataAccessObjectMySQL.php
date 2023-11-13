f4940ec1e6c408e9df7baddfefdb0e31188f20a0
tiago-py <tiago.braga0012@gmail.com>
2023-11-13 16:39:26 -0300

    fix: Implementando renderização das transações.

--- database/dao/TransactionDataAccessObjectMySQL.php
+++ database/dao/TransactionDataAccessObjectMySQL.php
@@ -39,43 +39,48 @@ class TransactionDataAccessObjectMySQL implements ITransactionDataAccessObject {
     public function joinTransactions() {
         try {
             $query = "SELECT 
-                        p.product_id,
-                        p.name, 
-                        p.cost, 
-                        p.unit_price, 
-                        p.min_stock, 
-                        c.name AS category_name, 
-                        s.name AS supplier_name,
-                        (COALESCE(st.purchase_sum, 0) - COALESCE(bt.sale_sum, 0)) AS current_stock,
-                        p.created_at,
-                        p.updated_at
-                    FROM 
-                        db_controle_de_estoque.tb_products AS p
-                    JOIN 
-                        db_controle_de_estoque.tb_categories AS c ON p.category_id = c.category_id
-                    JOIN 
-                        db_controle_de_estoque.tb_suppliers AS s ON p.supplier_id = s.supplier_id
-                    LEFT JOIN (
-                        SELECT 
-                            product_id,
-                            SUM(quantity) AS purchase_sum
+                            p.product_id,
+                            p.name, 
+                            p.cost, 
+                            p.unit_price, 
+                            p.min_stock, 
+                            c.name AS category_name, 
+                            s.name AS supplier_name,
+                            (COALESCE(st.purchase_sum, 0) - COALESCE(bt.sale_sum, 0)) AS current_stock,
+                            p.created_at,
+                            p.updated_at,
+                            CASE 
+                                WHEN st.purchase_sum IS NOT NULL THEN 'Compra'
+                                WHEN bt.sale_sum IS NOT NULL THEN 'Venda'
+                            END AS type
                         FROM 
-                            db_controle_de_estoque.tb_suppliers_transactions
-                        GROUP BY 
-                            product_id
-                    ) AS st ON p.product_id = st.product_id
-                    
-                    LEFT JOIN (
-                        SELECT 
-                            product_id,
-                            SUM(quantity) AS sale_sum
-                        FROM 
-                            db_controle_de_estoque.tb_buyers_transactions
-                        GROUP BY 
-                            product_id
-                    ) AS bt ON p.product_id = bt.product_id
-                    
-                    ORDER BY p.product_id ASC;";
+                            db_controle_de_estoque.tb_products AS p
+                        JOIN 
+                            db_controle_de_estoque.tb_categories AS c ON p.category_id = c.category_id
+                        JOIN 
+                            db_controle_de_estoque.tb_suppliers AS s ON p.supplier_id = s.supplier_id
+                        LEFT JOIN (
+                            SELECT 
+                                product_id,
+                                SUM(quantity) AS purchase_sum
+                            FROM 
+                                db_controle_de_estoque.tb_suppliers_transactions
+                            GROUP BY 
+                                product_id
+                        ) AS st ON p.product_id = st.product_id
+                        
+                        LEFT JOIN (
+                            SELECT 
+                                product_id,
+                                SUM(quantity) AS sale_sum
+                            FROM 
+                                db_controle_de_estoque.tb_buyers_transactions
+                            GROUP BY 
+                                product_id
+                        ) AS bt ON p.product_id = bt.product_id
+                        
+                        ORDER BY p.product_id ASC;";
+                        
             $statement = $this->connection->prepare($query);
             $statement->execute();
 
